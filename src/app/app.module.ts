1b9d6d796c9b74ef2f92c24ee7c89152d0064208
Artem Kurochkin <a.kurochkin@sigma-it.ru>
2019-11-14 15:47:34 +0500

    fix верстки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 
-import { MainComponent } from './main/component/main.component';
+import { MainComponent } from './main/components/main/main.component';
 import { MainModule } from './main/main.module';
 import { IndexModule } from './index/index.module';
 import { CommonModule } from '@common/common.module';
 import { DetailModule } from './detail/detail.module';
+import { HeaderComponent } from './main/components/header/header.component';
 
 @NgModule({
 	declarations: [],
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MainComponent } from './component/main.component';
+import { MainComponent } from './components/main/main.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
 	declarations: [
-		MainComponent
+		MainComponent,
+		HeaderComponent
 	],
 	imports: [
 		RouterModule,
